4a244a389ec4ce2f753fc1c516b4cea52b8cc611
iv4no <iv4no@LAPTOP-PO6T9U2M>
2025-10-30 14:05:05 +0300

    Добавьте файлы проекта.

---
+++ BarcodeFinder/BarcodeReader.h
@@ -0,0 +1,32 @@
+#ifndef BARCODEREADER_H
+#define BARCODEREADER_H
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <vector>
+
+class BarcodeReader {
+private:
+    const std::vector<std::string> LEFT_PATTERNS;
+    const std::vector<std::string> RIGHT_PATTERNS;
+
+public:
+    BarcodeReader();
+    std::string detectAndDecodeBarcode(const std::string& imagePath);
+
+private:
+    cv::Mat preprocessImage(const cv::Mat& image);
+    cv::Rect detectBarcodeRegion(const cv::Mat& binary);
+    std::string decodeBarcode(const cv::Mat& barcodeROI);
+    std::vector<int> getIntensityProfile(const cv::Mat& binary);
+    std::vector<int> normalizeProfile(const std::vector<int>& profile);
+    std::string decodeEAN13WithWidth(const std::vector<int>& profile, int width);
+    std::string decodeEAN8(const std::vector<int>& profile, int width);
+    int findStartMarkerWithWidth(const std::vector<int>& profile, int width);
+    int findCenterMarkerWithWidth(const std::vector<int>& profile, int start, int width);
+    int decodeDigitWithWidth(const std::vector<int>& profile, int start,
+    const std::vector<std::string>& patterns, int width);
+    int hammingDistance(const std::string& s1, const std::string& s2);
+};
+
+#endif
